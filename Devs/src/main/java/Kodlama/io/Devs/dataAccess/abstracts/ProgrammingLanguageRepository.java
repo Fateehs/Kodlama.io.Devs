0729afecf45d682a0bcff17bc5d7bba6c3a4dc7f
Fatih Enes Selvi <benzer1010@gmail.com>
2022-11-11 23:21:08 +0300

    JpaRepository added

---
+++ Devs/src/main/java/Kodlama/io/Devs/dataAccess/abstracts/ProgrammingLanguageRepository.java
@@ -0,0 +1,11 @@
+package Kodlama.io.Devs.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import Kodlama.io.Devs.entities.concretes.ProgrammingLanguage;
+
+@Repository
+public interface ProgrammingLanguageRepository extends JpaRepository<ProgrammingLanguage, Integer> {
+	public ProgrammingLanguage getByName(String name);
+}
