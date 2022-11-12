41425238822321a8e2dc4d28fe83af5539c565ac
Fatih Enes Selvi <benzer1010@gmail.com>
2022-11-12 13:55:09 +0300

    ModelMapper added

---
+++ Devs/src/main/java/Kodlama/io/Devs/DevsApplication.java
@@ -0,0 +1,19 @@
+package Kodlama.io.Devs;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+}
